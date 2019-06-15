a2b3cb382a0bc18400ddfe34dbd1c23d0ff4b33c
Jayraj Kachariya <jayrajkachariya1@gmail.com>
2019-06-15 23:32:14 +0530

    make it better

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
-// let devURL = 'http://localhost:5000/get-images/';
-let prodURL = 'https://image-basket.herokuapp.com/get-images/';
+let devURL = 'http://localhost:5000/get-images/';
+// let prodURL = 'https://image-basket.herokuapp.com/get-images/';
 
 const fetchImages = async ({ query, page }) => {
-  const res = await fetch(prodURL, {
+  const res = await fetch(devURL, {
     method: 'post',
     body: JSON.stringify({
       search: query,
