253249fbc78cf6d4c5b8b19041b2a99d411502e7
Jayraj Kachariya <jayrajkachariya1@gmail.com>
2019-06-15 23:10:30 +0530

    initial commit .1

--- server.js
+++ server.js
@@ -28,9 +28,10 @@ const fetchImages = async query => {
   });
 };
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon(__dirname + '/build/favicon.ico'));
+
 app.get('/ping', function(req, res) {
   return res.send('pong');
 });
